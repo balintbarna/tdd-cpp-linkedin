ecb17930e8079dfb48710b5c1fe0a73588570b08
Balint <balint@kovari.cc>
2024-01-18 10:48:28 +0100

    add checkout class

---
+++ Checkout.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+
+class Checkout {
+public:
+    Checkout() {}
+};
---
+++ CheckoutTests.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <gtest/gtest.h>
+
+#include "Checkout.hpp"
+
+
+using namespace std;
+
+
+TEST(CheckoutTests, CanInstantiateCheckout) {
+    Checkout co;
+}
