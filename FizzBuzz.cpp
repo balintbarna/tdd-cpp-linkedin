e32ca764126df3ea333ce4aef3002d28beb9c35d
Balint <balint@kovari.cc>
2024-01-18 09:45:34 +0100

    add basic source and tasks config file

---
+++ FizzBuzz.cpp
@@ -0,0 +1,13 @@
+// FizzBuzz kata:
+// Write a function that receives and prints a passed in number.
+// For multiples of 3 it prints "Fizz"
+// For multiples of 5 it prints "Buzz"
+// For multiples of both it prints "FizzBuzz"
+
+
+# include <gtest/gtest.h>
+
+
+TEST(FizzBuzzTest, doesPass) {
+    ASSERT_TRUE(true);
+}
