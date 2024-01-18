222dc24f5d0c5a7badb90cd42be9e2b14c82bb9a
Balint <balint@kovari.cc>
2024-01-18 09:52:27 +0100

    generalize with more tests

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -9,11 +9,17 @@
 
 
 std::string fizzBuzz(int value) {
-    return "1";
+    return std::to_string(value);
 }
 
 
 TEST(FizzBuzzTest, returns1With1PassedIn) {
     std::string result = fizzBuzz(1);
     ASSERT_STREQ("1", result.c_str());
+}
+
+
+TEST(FizzBuzzTest, returns2With2PassedIn) {
+    std::string result = fizzBuzz(2);
+    ASSERT_STREQ("2", result.c_str());
 }
